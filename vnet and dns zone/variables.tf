eb28c842844935924aab32412416759912cd2b4a
manoj h <manoj.h1@brillio.com>
2023-09-22 14:35:21 +0530

    tf script of vnet and dns zone

---
+++ vnet and dns zone/variables.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+  type    = string
+  default = "61323407-b144-4eac-883f-cc64a89b82e4"
+}
+variable "client_id" {
+  type    = string
+  default = "a2cd9f5b-9606-4380-a30c-8185fde63936"
+}
+variable "tenant_id" {
+  type    = string
+  default = "97984c2b-a229-4609-8185-ae84947bc3fc"
+}
+variable "client_secret" {
+  type    = string
+  default = "pIM8Q~ZsliPrN1AMQT7sfrcGq6haoY3RtAJAgbh~"
+}
+variable "object_id" {
+  type    = string
+  default = "31786876-0f45-41ad-ae6a-7fdf9be28a29"
+}
